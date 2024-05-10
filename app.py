6417b980b6fd23cc6de0657524bc6352f38779c8
andresuryaatmojo <andresuryaatmojo@gmail.com>
2024-05-11 05:50:40 +0700

    Update app.py to handle command-line arguments

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import sys
+
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python app.py [name]")
+        sys.exit(1)
+    
+    nama = sys.argv[1]
     print("Selamat datang di aplikasi command-line sederhana.")
-    nama = input("Masukkan nama Anda: ")
     print(f"Halo, {nama}! Selamat belajar Jenkins dengan Python.")
 
 if __name__ == "__main__":
