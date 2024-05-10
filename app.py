27f44f318588e93736638db830cb6167ab0990af
andresuryaatmojo <andresuryaatmojo@gmail.com>
2024-05-11 05:12:52 +0700

    Initial commit of command-line application

---
+++ app.py
@@ -0,0 +1,7 @@
+def main():
+    print("Selamat datang di aplikasi command-line sederhana.")
+    nama = input("Masukkan nama Anda: ")
+    print(f"Halo, {nama}! Selamat belajar Jenkins dengan Python.")
+
+if __name__ == "__main__":
+    main()
